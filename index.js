fb9159458d7bd584280dff688eb5754dfd16f49c
chourasiyaaryan225 <chourasiyaaryan225@gmail.com>
2026-05-11 16:40:00 +0530

    (main):Initial Setup

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+app.get('/',(req,res)=>{
+    res.status(200).json({message:'hello from server.'})
+})
+app.listen(3000,()=>console.log('server is listening on port 3000'));
